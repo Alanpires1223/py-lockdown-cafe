a6668a4f2557dfc8b9011f42332d68d06bf7b3df
Alan <alanpires1223@gmail.com>
2025-09-18 10:40:03 -0300

    feat: Adiciona anotações de tipo e corrige erros de estilo

--- app/errors.py
+++ app/errors.py
@@ -5,18 +5,18 @@ class VaccineError(Exception):
 
 # Erro quando o visitante não está vacinado
 class NotVaccinatedError(VaccineError):
-    def __init__(self, message="Visitante não está vacinado"):
+    def __init__(self, message: str = "Visitante não está vacinado") -> None:
         super().__init__(message)
 
 
 # Erro quando a vacina do visitante está vencida
 class OutdatedVaccineError(VaccineError):
-    def __init__(self, message=(
-            "Vacina do visitante está vencida")):
+    def __init__(self, message: str = (
+            "Vacina do visitante está vencida")) -> None:
         super().__init__(message)
 
 
 class NotWearingMaskError(Exception):
-    def __init__(self, message=(
-            "Visitante não está usando máscara")):
+    def __init__(self, message: str = (
+            "Visitante não está usando máscara")) -> None:
         super().__init__(message)
