d25ece7efdd88832111b04fbce9520b188e90f2b
Alan <alanpires1223@gmail.com>
2025-09-18 11:55:22 -0300

    feat: Adiciona anotações de tipo e corrige erros de estilo

--- app/main.py
+++ app/main.py
@@ -9,11 +9,11 @@ def go_to_cafe(friends: list, cafe: Cafe) -> str:
         try:
             cafe.visit_cafe(friend)
         except VaccineError:
-            return "Todos os amigos devem estar vacinados"
+            return "All friends should be vaccinated"
         except NotWearingMaskError:
             masks_to_buy += 1
 
     if masks_to_buy > 0:
-        return f"Amigos devem comprar {masks_to_buy} máscaras"
+        return f"Friends should buy {masks_to_buy} masks"
 
-    return f"Amigos podem ir ao {cafe.name}"
+    return f"Friends can go to {cafe.name}"
