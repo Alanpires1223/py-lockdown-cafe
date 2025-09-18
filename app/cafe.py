a1774d2b897f33412f6e7f4d536126f99238ed0c
Alan <alanpires1223@gmail.com>
2025-09-18 10:26:41 -0300

    feat: Adiciona anotações de tipo e corrige erros de estilo

--- app/cafe.py
+++ app/cafe.py
@@ -1,13 +1,16 @@
 import datetime
-from errors import (NotVaccinatedError, OutdatedVaccineError,
-                    NotWearingMaskError)
+from errors import (
+    NotVaccinatedError,
+    OutdatedVaccineError,
+    NotWearingMaskError
+)
 
 
 class Cafe:
-    def __init__(self, name):
+    def __init__(self, name: str) -> None:
         self.name = name
 
-    def visit_cafe(self, visitor: dict):
+    def visit_cafe(self, visitor: dict) -> str:
         # Verifica se está vacinado
         if "vaccine" not in visitor:
             raise NotVaccinatedError("Visitante não possui vacina registrada")
